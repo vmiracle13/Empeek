816ad9dfa82055ada3c70712703e038779bafe58
Vika Borysovskaya <vmiracle13@gmail.com>
2018-02-22 20:26:54 +0300

    add styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,57 +2,135 @@ import React, {Component} from 'react';
 import './App.css';
 import Form from './../Form/Form';
 import Item from './../Item/Item';
+import Comment from './../Comment/Comment';
 
 export default class App extends Component {
     constructor(props) {
         super(props);
-        this.state = JSON.parse(localStorage.getItem('itemList')) ? {items: JSON.parse(localStorage.getItem('itemList'))} : { items: []};
+        this.state = JSON.parse(localStorage.getItem('itemList')) ? {items: JSON.parse(localStorage.getItem('itemList'))} : {items: []};
         this.handleCreateItem = this.handleCreateItem.bind(this);
+        this.handleCreateComment = this.handleCreateComment.bind(this);
         this.handleDeleteItem = this.handleDeleteItem.bind(this);
+        this.handleActivate = this.handleActivate.bind(this);
     }
 
-    handleCreateItem(item) {
-        this.setState({items: this.state.items.push(item)});
-        this.saveIntoLocalStorage(this.state.items);
+    handleCreateItem(name) {
+        const notUnique = this.state.items.some((item) => {
+            return item.name === name;
+        });
+
+        if (notUnique) {
+            alert('Duplicate item can not be saved');
+            return;
+        }
+
+        const item = {
+            name: name,
+            id: this.state.items.length,
+            active: !this.state.items.length,
+            comments: []
+        };
+
+        this.setState({items: (this.state.items).concat([item])}, () => {
+            this.saveIntoLocalStorage(this.state.items);
+        });
+    }
+
+    handleCreateComment(text) {
+        const updatedItems = this.state.items.map((item) => {
+            if (item.active) {
+
+                const commentObj = {
+                    id: item.comments.length,
+                    text: text
+                };
+
+                item.comments.push(commentObj);
+            }
+
+            return item;
+        });
+
+        this.setState({items: updatedItems}, () => {
+            this.saveIntoLocalStorage(updatedItems);
+        });
     }
 
     saveIntoLocalStorage(array) {
-        localStorage.setItem( 'itemList', JSON.stringify(array) );
+        localStorage.setItem('itemList', JSON.stringify(array));
     }
 
-    handleDeleteItem(item) {
-        if ( this.state.items.indexOf(item) !== -1 ) {
-            let a = this.state.items.slice();
-            a.splice(a.indexOf(item), 1);
+    handleDeleteItem(name) {
+        const updatedItems = this.state.items.filter((item, i) => {
+            if (item.name !== name) {
+                return item;
+            }
+        });
 
-            console.log(a);
-
-            this.setState = {items: a};
-            this.saveIntoLocalStorage(a);
-        } else {
-            alert('Please, re-check the data');
+        if (updatedItems.length) {
+            updatedItems[0].active = true;
         }
+
+        this.setState({items: updatedItems}, () => {
+            this.saveIntoLocalStorage(updatedItems);
+        });
+    }
+
+    handleActivate(item) {
+        this.activeItem.active = false;
+        this.activeItem = item;
+        this.activeItem.active = true;
+        this.setState({items: this.state.items});
+        this.saveIntoLocalStorage(this.state.items);
     }
 
     render() {
+        this.activeItem = this.state.items.filter((item) => {
+            if (item.active) return item;
+        })[0];
+
         return (
             <div className="app">
-                <Form onHandleCreateItem={this.handleCreateItem} />
+                <aside className="aside">
+                    <h2 className="aside_title">Dairy app</h2>
+                    <p className="aside_text">Comment with no sense</p>
+                </aside>
 
-                {this.state.items.length > 0 &&
-                    <div>
-                    <ul className="list">
-                        {this.state.items.map((item, i) => {
-                            return (
-                                <li key={item}>
-                                    <Item item={item} onHandleDelete={this.handleDeleteItem}/>
-                                </li>
-                            );
-                        })
-                        }
-                    </ul>
+                <div className="content-block">
+                    <div className="items-wrap">
+                        <h2 className="items-wrap__h2">Items</h2>
+                        <Form onHandleCreateItem={this.handleCreateItem}/>
+
+                        {this.state.items.length > 0 &&
+                        <div>
+                            <ul className="item-list">
+                                {this.state.items.map((item) => {
+                                    return (
+                                        <li key={item.name} className="item-list__elem">
+                                            <Item item={item}
+                                                  onHandleDelete={this.handleDeleteItem}
+                                                  onActivate={this.handleActivate}
+                                            />
+                                        </li>
+                                    );
+                                })
+                                }
+                            </ul>
+                        </div>}
                     </div>
-                }
+
+                    <div className="comment-wrap">
+                        {this.state.items.length > 0 &&
+                        <div>
+                            {<h2 className="comment-wrap__h2">Comments #{this.activeItem.id + 1}</h2>}
+                            {<Comment
+                                comments={this.activeItem.comments}
+                                onHandleCreateComment={this.handleCreateComment}
+                            />}
+                        </div>
+                        }
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,27 +1,57 @@
 import React from 'react';
 import {Component} from 'react';
+import './Form.css';
+import isEmpty from './../../index';
 
 export default class Form extends Component {
     constructor(props) {
         super(props);
         this.state = {value: ''};
-        this.handleChange = (event) => {this.setState({ value: event.target.value});};
-        this.handleSubmit = () => {
-            if (!this.state.value) {
-                alert('The item is empty.');
+
+        this.handleChange = (event) => {
+            this.setState({value: event.target.value});
+        };
+        this.handleSubmit = (event) => {
+            if (isEmpty(this.state.value)) return;
+
+            this.props.onHandleCreateItem(this.state.value);
+        };
+
+        this.handleKeyDown = (event) => {
+            const e = event || window.event;
+
+            if (e.ctrlKey && e.keyCode === 13) {
+                if (isEmpty(this.state.value)) return;
+
+                this.props.onHandleCreateItem(this.state.value);
+                this.setState({value: ''});
                 return;
             }
 
-            this.props.onHandleCreateItem(this.state.value);
+            if(e.keyCode === 13) {
+                alert('Use Ctrl + Enter to save an item');
+                event.preventDefault();
+            }
         };
     }
 
     render() {
         return (
-            <form className="add" name="add" onSubmit={this.handleSubmit}>
-                <input className="inputField" type="text" placeholder="Type name here ..."
-                       onChange={this.handleChange}/>
-                <input className="inputField" type="submit" value="Add new"/>
+            <form className="add-item-form"
+                  name="add-item-form"
+                  onSubmit={this.handleSubmit}
+                  onKeyDown={this.handleKeyDown}
+            >
+                <input className="add-item__input"
+                       type="text"
+                       placeholder="Type name here..."
+                       value={this.state.value}
+                       onChange={this.handleChange}
+                />
+                <input className="add-item__input-submit"
+                       type="submit"
+                       value="Add new"
+                />
             </form>
         );
     }
